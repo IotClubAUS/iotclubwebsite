db0693534327773c7fdad88a5d4edf28e82ef528
adam <adam_serhan@hotmail.com>
2026-06-16 23:57:59 +0400

    fix deployment issues

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -47,6 +47,7 @@ export default function TeamPage() {
      
 
       <div className="max-w-6xl mx-auto px-6 py-16">
+    </div>
 
         {/* ROLE FILTERS */}
 <div className="flex flex-wrap gap-2 mb-10">
@@ -279,6 +280,5 @@ export default function TeamPage() {
         </div>
       </div>
     </div>
-     </div>
   );
 }
